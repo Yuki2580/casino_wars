dfa61a10d412f111fa9e3fe01311f56d592353d7
Yuki2580 <masamune5071@gmail.com>
2024-06-28 22:29:19 +0900

    fix: unnecessary files

--- casino_wars/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TWMfCzFzTalzth3cqRiRaDLhCqNJ7koDSxH+gDte8YM=\"\n}"
--- city_nft_frame/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DpmTJgRptPu4Kl1YLy87bpMPror/GmzIpLD5U/QRSUg=\"\n}"
